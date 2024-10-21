60d1366d35057cb4f9b82222c9ba7fad4ee61b55
Illarion Koperski <dev@illarionvk.com>
2024-10-21 15:01:59 +0200

    add fish shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,7 +13,27 @@ fish_add_path -m ~/heroku/bin
 fish_add_path -m ~/node/bin
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
-    starship init fish | source
+  set -gx EDITOR nvim
+
+  # Commands to run in interactive sessions can go here
+  fish_vi_key_bindings
+  starship init fish | source
+
+  function switchAllPanesToFolderInTmux
+    cd $argv[1]
+    set paneIndexes (tmux list-panes -F "#{pane_index}")
+    for i in $paneIndexes
+      tmux send-keys -t $i "cd \"$(pwd)\" && clear" Enter
+    end
+    tmux select-pane -t $paneIndexes[1]
+  end
+  alias cdpanes=switchAllPanesToFolderInTmux
+
+  alias vim="nvim"
+
+  function timelogs
+    vim -O2 ~/Tresorit/Main/timelog-macbook.txt ~/Tresorit/Main/timelog-imac.txt
+  end
+
+  abbr --add ai '~/core/dev/aider.sh'
 end
