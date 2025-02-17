0c6dabade0b44551cdde97725b13aed4cc2b9582
Illarion Koperski <dev@illarionvk.com>
2025-02-17 20:34:27 +0100

    Add gnu parallel abbreviations

--- .config/fish/conf.d/20-abbreviations.fish
+++ .config/fish/conf.d/20-abbreviations.fish
@@ -1,1 +1,5 @@
   abbr --add rg 'rg --smart-case'
+
+  abbr --add argdo 'parallel --will-cite --jobs=100% --halt=soon,fail=10% --no-run-if-empty --line-buffer'
+  
+  abbr --add xargdo 'parallel --will-cite --jobs=100% --halt=soon,fail=10% --no-run-if-empty --xargs --max-args=20'
