c1cf0ea0d34c64b9592a7b747a9adabcaf423bbf
Illarion Koperski <dev@illarionvk.com>
2025-03-06 16:30:15 +0100

    Set model aliases

--- .config/fish/functions/ai.fish
+++ .config/fish/functions/ai.fish
@@ -2,9 +2,13 @@ function ai --wraps aider --description 'Shortcut for aider'
   #set -f openai_token (op read 'op://Shared/OpenAI API Illarion/credential' | tr -d "[:blank:]")
   #set -f anthropic_token (op read 'op://Shared/Anthropic API Illarion/credential' | tr -d "[:blank:]")
   set -x OPENROUTER_API_KEY (op read 'op://Shared/OpenRouter API Illarion/credential' | tr -d "[:blank:]")
-  set -x AIDER_MODEL 'openrouter/deepseek/deepseek-r1'
-  set -x AIDER_EDITOR_MODEL 'openrouter/anthropic/claude-3.5-sonnet'
-  set -x AIDER_WEAK_MODEL 'openrouter/anthropic/claude-3-5-haiku'
 
-  command aider --no-auto-commits --architect --multiline $argv
+  command aider --no-auto-commits --multiline \
+    --alias 'deepseek:openrouter/deepseek/deepseek-r1' \
+    --alias 'haiku:openrouter/anthropic/claude-3-5-haiku' \
+    --alias 'o1:openrouter/openai/o1' \
+    --alias 'sonnet:openrouter/anthropic/claude-3.7-sonnet' \
+    --architect \
+    --model deepseek --editor-model sonnet --weak-model haiku \
+    $argv
 end
