584b6a565f418c0042650a675c75eb95087c406f
Illarion Koperski <dev@illarionvk.com>
2025-03-19 16:14:55 +0100

    Add demarkdown abbreviation

--- .config/fish/conf.d/20-abbreviations.fish
+++ .config/fish/conf.d/20-abbreviations.fish
@@ -1,7 +1,9 @@
-  abbr --add rg 'rg --smart-case'
+abbr --add rg 'rg --smart-case'
 
-  abbr --add argdo 'parallel --will-cite --jobs=100% --halt=soon,fail=10% --no-run-if-empty --line-buffer'
-  
-  abbr --add xargdo 'parallel --will-cite --jobs=100% --halt=soon,fail=10% --no-run-if-empty --xargs --max-args=20'
-  
-  abbr --add vargs 'xargs -o nvim'
+abbr --add argdo 'parallel --will-cite --jobs=100% --halt=soon,fail=10% --no-run-if-empty --line-buffer'
+
+abbr --add xargdo 'parallel --will-cite --jobs=100% --halt=soon,fail=10% --no-run-if-empty --xargs --max-args=20'
+
+abbr --add vargs 'xargs -o nvim'
+
+abbr --add demarkdown 'pandoc -f markdown -t plain --wrap=none'
